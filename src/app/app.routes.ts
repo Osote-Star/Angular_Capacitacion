24d9708589badce410d6aa31652b173fee0d7fb5
Osote-Star <5321100090@correcaminos.uts.edu.mx>
2024-11-06 23:25:31 -0700

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter } from '@angular/router';
+import { TareaListComponent } from './components/tarea-list/tarea-list.component';
+import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'tareas', component: TareaListComponent},
+    {path: 'usuarios', component: UsuarioListComponent},
+    {path: '**', redirectTo: '/usuario', pathMatch: 'full'}
+];
+
+export const appRouterProviders = [provideRouter(routes)];
